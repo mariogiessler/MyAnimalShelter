951bc2ddfd77df9c549c5df8bed97e6f1286f33c
mariogiessler <mgiessler@live.de>
2019-12-13 09:23:48 +0100

    Initial commit

---
+++ src/Control/Controller.java
@@ -0,0 +1,9 @@
+package Control;
+import View.UserWindow;
+
+public class Controller {
+
+    public static void main(String[] args) {
+        UserWindow window = new UserWindow();
+    }
+}
---
+++ src/View/UserWindow.java
@@ -0,0 +1,14 @@
+package View;
+
+import javax.swing.*;
+
+public class UserWindow extends JFrame {
+    private static final long serialVersionUID = 1L;
+
+    public UserWindow(){
+        setTitle("Tierheim Moabit");
+        setSize(1200,800);
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+}
