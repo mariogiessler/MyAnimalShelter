9e5d3b27654879a52f6363babd82ba9c3a31ffe2
mgiessler <mgiessler@live.de>
2019-12-22 20:31:56 +0100

    final

--- src/view/styles/MyButton.java
+++ src/view/styles/MyButton.java
@@ -4,6 +4,10 @@ import controll.Controller;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 // design all buttons for the programm in a custom way
 
